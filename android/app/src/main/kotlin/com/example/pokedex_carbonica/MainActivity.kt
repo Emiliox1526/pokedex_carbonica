18de1a7dbf62c8ca358bfcbed99a6e106b9dc2e8
PavelR20 <parv0001@ce.pucmm.edu.do>
2025-12-02 19:49:46 -0400

    cambio de idioma el minijuego de pokemon, falta mapa y compartir pokemon card

--- android/app/src/main/kotlin/com/example/pokedex_carbonica/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pokedex_carbonica
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/utils/utils.dart
+++
@@ -1,4 +0,0 @@
-/// Barrel file for core utilities.
-export 'sprite_utils.dart';
-export 'string_utils.dart';
-export 'type_utils.dart';
